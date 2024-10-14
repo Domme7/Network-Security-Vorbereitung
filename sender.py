32809923baf911559005b9391d75536b1ea629bd
domme7 <dominik.hu@outlook.de>
2024-10-14 19:12:54 +0200

    sende Programm gemacht

---
+++ sender.py
@@ -0,0 +1,16 @@
+import socket
+import base64
+
+# lese Datei 
+with open ("robotergesetze.txt", "r", encoding="utf-8") as file:
+    data = file.read()
+
+# kodiere Daten
+encoded_data = base64.b64encode(data.encode())
+
+# erstelle TCP-Socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(('localhost', 65432)) # verbindet mit Programm B (Empfänger)
+    s.senall(encoded_data) # senden der Daten
+    print("Daten gesendet.")
+    
